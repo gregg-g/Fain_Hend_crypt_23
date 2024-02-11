336eef24e2fc9af8bef561b4f2aff3187eb3ab4c
gregg-g <gregg_griffenhagen@me.com>
2024-02-11 07:43:48 -0700

    edit for flow and comments

--- qtr_crypt_23.R
+++ qtr_crypt_23.R
@@ -2,16 +2,15 @@ library(ggplot2)
 library(magrittr)
 library(tidyverse)
 library(nnet)
+library(emmeans)
 
-data <- read.csv(file="crypt_data.csv", colClasses = c("integer",
-                                                       "NULL",
+data <- read.csv(file="crypt_data_2.csv", colClasses = c("NULL",
                                                        "factor",
-                                                       "NULL",
-                                                       "factor"))
+                                                       "factor",
+                                                       "NULL"))
 head(data)
-str(data)
-     #check that weight between groups is not different
-t.test(wt ~ brd, data = data)
+str(data); levels(data$brd); levels(data$lat)
+     
 
      #plot laterality by breed
 p1 <- ggplot(data=data, aes(brd, fill=lat))
@@ -20,9 +19,9 @@ ggsave("plot1", device="png")
 
 p2 <- ggplot(data=data, aes(lat, fill=brd))
 p2 + geom_bar(position="stack", color="black") +
-  scale_fill_manual(name="Breed",
-                    values=c("grey","white"),
-                    labels=c("Other", "Quarterhorse")) +
+ # scale_fill_manual(name="Breed",
+#                  values=c("grey","white"),
+ #                   labels=c("Other", "Quarterhorse")) +
   theme_classic() +
   labs(x="Laterality", y="Total Cases")
 ggsave("plot2", device="png")
@@ -33,33 +32,15 @@ cont_table
 
      #are there any significant differences?
 chisq.test(cont_table)
-mosaicplot(t(cont_table))
+fisher.test(cont_table, simulate.p.value = TRUE) #there are differences in the distribution between groups
+mosaicplot(t(cont_table)) #not super helpful or particularly legible
 
-     #combine into 3 groups (L, R, Bilat)
-data_small <- data %>%
-  mutate(lat_group = substr(lat, 1, 1))
 
-     #create new contingency table with 3 groups
-cont_table_2 <- table(data_small$brd, data_small$lat_group)
-cont_table_2
-
-     #check for significant differences
-chisq.test(cont_table_2)
-fisher.test(cont_table_2)
-mosaicplot(t(cont_table_2))
-
-p3 <- ggplot(data=data_small, aes(lat_group, fill=brd))
-p3 + geom_bar(position="stack", color="black") +
-  scale_fill_manual(name="Breed",
-                    values=c("grey","white"),
-                    labels=c("Other", "Quarterhorse")) +
-  theme_classic() +
-  labs(x="Laterality", y="Total Cases")
-ggsave("plot3", device="png")                                                   
+               
 
   #need regression model to get OR for individual laterality
      #create tall data from contingency table
-t1 <- as.data.frame.table(cont_table_2)
+t1 <- as.data.frame.table(cont_table)
 colnames(t1) <- c("brd", "lat", "freq")
      #check to make sure table looks correct
 str(t1)
@@ -76,15 +57,40 @@ str(t1)
 
   #multinomial model
      #more appropriate model - but not comparing L to R
-model2 <- multinom(lat ~ brd, data = t1, weights = freq)
+
+t1$brd <- fct_relevel(t1$brd, "QH")  #relevel to compare all to QH
+
+model2 <- multinom(lat ~ brd, data = t1, weights = freq, Hess=T, model=T)
 summary(model2)
-exp(coef(model2))
+exp(coef(model2))   #calculate odds ratios and CI
 exp(confint(model2))
 
-     #relevel to compare L vs R
-t1$lat <- fct_relevel(t1$lat, "R")
+  #!!create forest plot for multinomial data!!
 
-model3 <- multinom(lat ~ brd, data = t1, weights = freq)                      
+   #check for significant differences between all pairs
+
+emmout <- emmeans(model2, "brd", by="lat")
+pairs(emmout)
+contrast(emmout, "trt.vs.ctrl") #same results narrowing down groups to QH vs. each
+
+     #relevel to compare L vs R
+
+t1$lat <- fct_relevel(t1$lat, "Rt")
+t2 <- filter(t1, lat != "Bilat")
+
+model3 <- multinom(lat ~ brd, data = t2, weights = freq)                      
 summary(model3)
 exp(coef(model3))
 exp(confint(model3))
+
+emmout2 <- emmeans(model3, "brd", by="lat")
+pairs(emmout2)
+contrast(emmout2, "trt.vs.ctrl")
+
+model4 <- glm(lat ~ brd, family=binomial, data=t2, weights=freq)
+summary(model4)
+exp(coef(model4))
+exp(confint(model4))
+emmeans(model4, "brd") %>%
+  pairs()
+emmeans(model4, "brd") %>% contrast("trt.vs.ctrl")
